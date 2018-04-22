4e45ac7b7b9a9724c55f9f2ec29e93e77552561e
Serghei Motpan <smotpan@tacitknowledge.com>
2018-04-22 19:53:04 +0300

    Add a spring example using spring fragments taglibs

---
+++ spring-security-samples/sp4-secure-view-fragments-using-security-taglibs/src/main/java/dvl/srg/springsecurity/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package dvl.srg.springsecurity.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
